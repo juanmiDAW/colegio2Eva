0b6d18c00545249bda379b0d977c82ad76b5d1d3
DAW <juanmiguel.valle@iesdonana.org>
2025-02-26 15:40:03 +0100

    notas finales mas o menos

--- resources/views/notas/index.blade.php
+++ resources/views/notas/index.blade.php
@@ -69,12 +69,17 @@
                                 </tbody>
                             </table>
                         </div>
-                        <div class="mt-6 text-center">
+                        <div class="mt-6 text-center ">
                             <a href="{{ route('notas.create') }}"
                                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                                 Crear un nuevo notas
                             </a>
                         </div>
+                        <div class="mt-6 text-center">
+                            <a href="{{ route('notas.finales') }}"
+                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                                Ver notas finales
+                            </a>
                     </div>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AlumnoController;
 use App\Http\Controllers\AsignaturaController;
 use App\Http\Controllers\NotaController;
 use App\Models\Asignatura;
+use App\Models\Nota;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
@@ -22,3 +23,8 @@ require __DIR__.'/auth.php';
 Route::resource('alumnos', AlumnoController::class);
 Route::resource('asignaturas', AsignaturaController::class);
 Route::resource('notas', NotaController::class);
+
+Route::get('/finales/index', function (){
+    $notas = Nota::with('asignatura')->get();
+  return view('notas.finales', ['notas'=> $notas]);  
+})->name('notas.finales');
